b28d7e4a6e73746d5b233fa800185dcb03e71c82
B1SDT317\Yasotha <yashoda@velankanigroup.com>
2017-08-10 14:28:37 +0530

    replay history

--- js/admin/replay_history.js
+++ js/admin/replay_history.js
@@ -630,27 +630,41 @@ $scope.alarmHistData = data;
       });
 
 
-
-        google.maps.event.addListener(marker,'click', (function(marker, i){
+      google.maps.event.addListener(marker,'click' ,(function(marker,i){
         return function(){
-          // console.log("value of: ");
-          var elist = new Array();
-          for(j=0;j< data.values.length;j++){
-          console.log(data.values[j].alarm_type);
-          checkEventFilter(data.values[j].alarm_type);
-          elist.push($scope.eventValue); 
-          }
+      console.log(checkEventFilter(data.values[i].alarm_type));
+      console.log($scope.eventValue);
+        contentString  = '<b><label>Event type:</label></b> '+$scope.eventValue+'</n><br><br> ';
+      //     console.log(elist);
 
-         console.log(_.uniq(elist));
 
-         contentString  = '<b><label>Event type:</label></b> '+_.uniq(elist)+'</n><br><br> ';
-          console.log(elist);
 
           infowindow.setContent(contentString);
-          infowindow.open(map, marker);
+           infowindow.open(map, marker);
+           }
+      })(marker,i));
 
-        }
-      }) (marker, i));
+
+      //   google.maps.event.addListener(marker,'click', (function(marker, i){
+      //   return function(){
+      //     // console.log("value of: ");
+      //     var elist = new Array();
+      //     for(j=0;j< data.values.length;j++){
+      //     console.log(data.values[j].alarm_type);
+      //     checkEventFilter(data.values[j].alarm_type);
+      //     elist.push($scope.eventValue); 
+      //     }
+
+      //    console.log(_.uniq(elist));
+
+      //    contentString  = '<b><label>Event type:</label></b> '+_.uniq(elist)+'</n><br><br> ';
+      //     console.log(elist);
+
+      //     infowindow.setContent(contentString);
+      //     infowindow.open(map, marker);
+
+      //   }
+      // }) (marker, i));
 
       }
   }
